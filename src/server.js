97fb53cf79871b49d8b6eae6d22c304bf0b0a65b
Caio Vidinha <caiovidinha@id.uff.br>
2024-01-22 16:06:34 -0300

    removendo emoji

--- src/server.js
+++ src/server.js
@@ -3,4 +3,4 @@ const dotenv = require('dotenv').config()
 const http = require('http')
 
 const server=http.createServer(app)
-server.listen(3333, () => console.log(`🚀 Servidor rodando na porta 3333`))
+server.listen(3333, () => console.log(`Servidor rodando na porta 3333`))
